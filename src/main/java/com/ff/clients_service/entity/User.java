d478963035e501ae45847f0a78d3f6cd6e85e328
Helder <hfernandes238@gmail.com>
2025-06-13 08:53:13 +0200

    Création Entity UserService

---
+++ src/main/java/com/ff/clients_service/entity/User.java
@@ -0,0 +1,22 @@
+package com.ff.clients_service.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(unique = true, nullable=false)
+    private String email;
+    @Column(unique = true, nullable=false)
+    private String password;
+}
