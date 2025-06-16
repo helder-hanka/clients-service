802271c158d303d39ecc99281e152683c138e779
Helder <hfernandes238@gmail.com>
2025-06-16 20:03:11 +0200

    Création DTO Auth

---
+++ src/main/java/com/ff/clients_service/dto/AuthRequest.java
@@ -0,0 +1,13 @@
+package com.ff.clients_service.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthRequest {
+    private String email;
+    private String password;
+}
